b900d4d7ffca959b75e19511bdf89321e09fc615
6Emirhan1 <emirhan.gunay@std.yildiz.edu.tr>
2025-02-07 16:51:51 +0300

    Add files via upload

---
+++ def palyndrome () :.py
@@ -0,0 +1,11 @@
+def palyndrome () :
+    if word.lower()==word2.lower() :
+         print("this is a palyndrom word")
+         
+
+
+
+word=input("please enter your word")
+word2=word[::-1]
+
+palyndrome()
